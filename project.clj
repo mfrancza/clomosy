c9525f79c8a4b43fda7a78a69ef3e57878a8e888
Matt Franczak <mfrancza@gmail.com>
2021-11-28 17:46:47 -0600

    Updated Project Info and Dependencies and Add lein-ancient

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clomosy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]])
+  :description "A modular synthesizer implemented in Clojure"
+  :url "https://github.com/mfrancza/clomosy"
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.5.640"]]
+  :plugins [[lein-ancient "1.0.0-RC4-SNAPSHOT"]])
