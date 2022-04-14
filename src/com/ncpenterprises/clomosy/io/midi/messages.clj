978bbc3cb09b342bfde7cc314849b9c4b56a86c6
Matt Franczak <mfrancza@gmail.com>
2022-04-14 15:43:09 -0500

    Document Message Protocols

--- src/com/ncpenterprises/clomosy/io/midi/messages.clj
+++ src/com/ncpenterprises/clomosy/io/midi/messages.clj
@@ -1,21 +1,22 @@
 (ns com.ncpenterprises.clomosy.io.midi.messages
+  "protocols for interacting with MIDI messages"
   (:import (javax.sound.midi ShortMessage)))
 
 (defprotocol ChannelMessage
   "Protocol for interacting with MIDI Channel Messages"
-  (channel-message? [message] "returns if the message is a Channel Message")
-  (channel [message] "returns the Channel the message is related to"))
+  (channel-message? [message] "if the message is a Channel Message")
+  (channel [message] "the Channel the message is related to"))
 
 (defprotocol NoteOff
   "Protocol for the NoteOff message "
-  (note-off? [message] "returns if the message is a Note Off Message ()")
-  (note-number [message])
-  (velocity [message]))
+  (note-off? [message] "if the message is a Note Off message")
+  (note-number [message] "the note number of the note being turned off")
+  (velocity [message] "the velocity the note is being turned off with"))
 
 (defprotocol NoteOn
-  (note-on? [message])
-  (note-number [message])
-  (velocity [message]))
+  (note-on? [message] "if the message is a Note On message")
+  (note-number [message] "the note number of the note being turned on")
+  (velocity [message] "the velocity the note is being turned on with"))
 
 (extend-type ShortMessage
   ChannelMessage
