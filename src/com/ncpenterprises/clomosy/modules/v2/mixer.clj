555bf5d58b9bbfcde257642bf3f8ae871448b4f2
Matt Franczak <mfrancza@gmail.com>
2022-04-14 17:38:19 -0500

    Fix Position of Function Documentation

--- src/com/ncpenterprises/clomosy/modules/v2/mixer.clj
+++ src/com/ncpenterprises/clomosy/modules/v2/mixer.clj
@@ -12,8 +12,8 @@
       {:outputs {:out (apply mixing-fn input-values)}})))
 
 (defn mixer
-  [channels mixing-fn]
   "returns a Module with the specified number of input channels as input-n starting with input-1 which outputs the mixing-fn applied to the values of the inputs"
+  [channels mixing-fn]
   (let [input-names (get-input-names channels)]
     (simple-v2-engine/map->Module {:update-fn    (get-mixer-update-fn mixing-fn input-names)
                                    :input-names  input-names
